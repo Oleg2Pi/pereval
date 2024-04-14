3e796a7d178ede8343d55d690ab1a7e2ceeeeab1
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-04-14 12:00:01 +0300

    Create view for API_create_pereval

--- pereval/data_of_pereval/views.py
+++ pereval/data_of_pereval/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import *
+from .serializers import PerevalSerializer
 
 
+class PerevalCreateViewset(viewsets.ModelViewSet):
+    '''Создание перевала'''
+
+    queryset = PerevalModel.objects.filter(id=0)
+    serializer_class = PerevalSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        serializer = PerevalSerializer(data=request.data)
+        response_data = {}
+
+        if serializer.is_valid():
+            serializer.save()
+            response_data = {
+                'status': 200,
+                'message': '',
+                'id': serializer.data.get('id')
+            }
+
+        elif status.HTTP_500_INTERNAL_SERVER_ERROR:
+            response_data = {
+                'status': 500,
+                'message': 'Ошибка подключения к базе данных',
+                'id': serializer.data.get('id')
+            }
+
+        elif status.HTTP_400_BAD_REQUEST:
+            response_data = {
+                'status': 400,
+                'message': 'Неверный запрос',
+                'id': serializer.data.get('id')
+            }
+
+        return Response(response_data)
